4c4c6ea2f264bce3902b45d26579cbd008c36891
Daniel Ibarra <dannibar201@gmail.com>
2025-03-26 18:27:32 -0400

    updated

--- src/test/java/com/example/AviaryService/AviaryServiceApplicationTests.java
+++ src/test/java/com/example/AviaryService/AviaryServiceApplicationTests.java
@@ -2,12 +2,13 @@ package com.example.AviaryService;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")  // Use a test profile
 class AviaryServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
