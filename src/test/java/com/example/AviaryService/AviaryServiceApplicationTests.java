8cee8ea5c564209f2ea2d030ab22bc1146d963be
Daniel Ibarra <dannibar201@gmail.com>
2025-03-26 13:55:50 -0400

    added new CI and branch

--- src/test/java/com/example/AviaryService/AviaryServiceApplicationTests.java
+++ src/test/java/com/example/AviaryService/AviaryServiceApplicationTests.java
@@ -1,13 +1,13 @@
 package com.example.AviaryService;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AviaryServiceApplicationTests {
-
-	@Test
-	void contextLoads() {
+@ActiveProfiles("test")
+	class AviaryServiceApplicationTests {
+		@Test
+		void contextLoads() {
+		}
 	}
-
-}
