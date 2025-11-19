72366db07d40df25482614a9df9eecb19019314c
Daniel Ibarra <dannibar201@gmail.com>
2025-11-19 12:36:46 -0500

    Added eyeball icon to reveal or hide password for users logining or registering, added autocmplete to username input box and password input box to allow save login to activate for browsers(not sure if this works)

---
+++ src/main/resources/static/js/signin.js
@@ -0,0 +1,22 @@
+// password-toggle.js
+document.addEventListener('DOMContentLoaded', function () {
+    const toggleButtons = document.querySelectorAll('.toggle-password');
+
+    toggleButtons.forEach(button => {
+        button.addEventListener('click', function () {
+            // Find the nearest password input (works even if you have multiple forms on one page later)
+            const passwordInput = this.closest('.password-wrapper').querySelector('input[name="password"]');
+            const icon = this.querySelector('i');
+
+            if (passwordInput.type === 'password') {
+                passwordInput.type = 'text';
+                icon.classList.remove('fa-eye-slash');
+                icon.classList.add('fa-eye');
+            } else {
+                passwordInput.type = 'password';
+                icon.classList.remove('fa-eye');
+                icon.classList.add('fa-eye-slash');
+            }
+        });
+    });
+});
